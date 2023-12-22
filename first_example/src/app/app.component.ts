6995c5ce24c7d0ef2abfeac5b9591fc45658fe66
Ahmed Moustafa <43710157+ahmedhmf@users.noreply.github.com>
2023-12-22 15:57:33 +0200

    checking in first working example

--- first_example/src/app/app.component.ts
+++ first_example/src/app/app.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { WebsocketService } from './websocket.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
-import { BrowserModule } from '@angular/platform-browser';
 import { Product } from './product.model';
 import { products } from './items.data';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {Cart} from "./cart.model";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, MatInputModule, MatCardModule, MatSelectModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, MatInputModule, MatCardModule, MatSelectModule, MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   items: Product[] = products;
+  cart: Cart[] = [];
+  total: number = 0;
+
+  addClick(item:Product):void{
+    const itemIndex =this.cart.findIndex(x=>x.item.id === item.id);
+    itemIndex < 0 ?this.cart.push({item,amount:1}) : this.cart[itemIndex].amount += 1;
+    this.calculateTotal();
+  }
+
+  removeItem(itemId: number){
+    const cartItemIndex = this.cart.findIndex(x=>x.item.id === itemId);
+    const cartItem = this.cart[cartItemIndex];
+    cartItem.amount ===1? this.cart.splice(cartItemIndex,1) : cartItem.amount -= 1;
+    this.calculateTotal();
+  }
+
+  private calculateTotal(){
+    this.total = 0;
+    this.cart.map(x=> this.total += x.amount * x.item.price);
+  }
 }
--- first_example/src/app/items.data.ts
+++ first_example/src/app/items.data.ts
@@ -2,6 +2,7 @@ import { Product } from "./product.model";
 
 export const products: Product[] = [
   {
+    id: 0,
     name:'Thermal Bottle',
     price: 3.10,
     picture: 'assets/images/ThermalBottle.webp',
@@ -9,6 +10,7 @@ export const products: Product[] = [
     new: false
   },
   {
+    id:1,
     name:'Thermal Bottle',
     price: 2.57,
     picture: 'assets/images/ThermalBottleAngular.webp',
@@ -16,6 +18,7 @@ export const products: Product[] = [
     new: false
   },
   {
+    id:2,
     name:'Thermal Bottle',
     price: 2.68,
     picture: 'assets/images/ThermalBottleBash.webp',
@@ -23,6 +26,7 @@ export const products: Product[] = [
     new: false
   },
   {
+    id:3,
     name:'Phone Skin',
     price: 20,
     picture: 'assets/images/PhoneSkinAngular.webp',
@@ -30,6 +34,7 @@ export const products: Product[] = [
     new: false
   },
   {
+    id:4,
     name:'Copy Book',
     price: 13.54,
     picture: 'assets/images/CopyBookAngular.webp',

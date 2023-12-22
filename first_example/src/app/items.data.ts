ed89f78d92d44ecf86e77e1580a840a56ee67bb2
Ahmed Moustafa <43710157+ahmedhmf@users.noreply.github.com>
2023-12-22 22:06:20 +0200

    defer

--- first_example/src/app/items.data.ts
+++ first_example/src/app/items.data.ts
@@ -41,4 +41,124 @@ export const products: Product[] = [
     type: 'CopyBook',
     new: false
   },
+  {
+    id: 0,
+    name:'Thermal Bottle',
+    price: 3.10,
+    picture: 'assets/images/ThermalBottle.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:1,
+    name:'Thermal Bottle',
+    price: 2.57,
+    picture: 'assets/images/ThermalBottleAngular.webp',
+    type: 'Water Bottle',
+    new: true
+  },
+  {
+    id:2,
+    name:'Thermal Bottle',
+    price: 2.68,
+    picture: 'assets/images/ThermalBottleBash.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:3,
+    name:'Phone Skin',
+    price: 20,
+    picture: 'assets/images/PhoneSkinAngular.webp',
+    type: 'Phone',
+    new: false
+  },
+  {
+    id:4,
+    name:'Copy Book',
+    price: 13.54,
+    picture: 'assets/images/CopyBookAngular.webp',
+    type: 'CopyBook',
+    new: false
+  },
+  {
+    id: 0,
+    name:'Thermal Bottle',
+    price: 3.10,
+    picture: 'assets/images/ThermalBottle.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:1,
+    name:'Thermal Bottle',
+    price: 2.57,
+    picture: 'assets/images/ThermalBottleAngular.webp',
+    type: 'Water Bottle',
+    new: true
+  },
+  {
+    id:2,
+    name:'Thermal Bottle',
+    price: 2.68,
+    picture: 'assets/images/ThermalBottleBash.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:3,
+    name:'Phone Skin',
+    price: 20,
+    picture: 'assets/images/PhoneSkinAngular.webp',
+    type: 'Phone',
+    new: false
+  },
+  {
+    id:4,
+    name:'Copy Book',
+    price: 13.54,
+    picture: 'assets/images/CopyBookAngular.webp',
+    type: 'CopyBook',
+    new: false
+  },
+  {
+    id: 0,
+    name:'Thermal Bottle',
+    price: 3.10,
+    picture: 'assets/images/ThermalBottle.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:1,
+    name:'Thermal Bottle',
+    price: 2.57,
+    picture: 'assets/images/ThermalBottleAngular.webp',
+    type: 'Water Bottle',
+    new: true
+  },
+  {
+    id:2,
+    name:'Thermal Bottle',
+    price: 2.68,
+    picture: 'assets/images/ThermalBottleBash.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    id:3,
+    name:'Phone Skin',
+    price: 20,
+    picture: 'assets/images/PhoneSkinAngular.webp',
+    type: 'Phone',
+    new: false
+  },
+  {
+    id:4,
+    name:'Copy Book',
+    price: 13.54,
+    picture: 'assets/images/CopyBookAngular.webp',
+    type: 'CopyBook',
+    new: false
+  },
 ]

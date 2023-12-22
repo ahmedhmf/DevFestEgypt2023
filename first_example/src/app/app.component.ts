44ab2e6c3ddebccfba8972275a8aeef4361eb28a
Ahmed Moustafa <43710157+ahmedhmf@users.noreply.github.com>
2023-12-22 13:11:08 +0100

    init

--- first_example/src/app/app.component.ts
+++ first_example/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { WebsocketService } from './websocket.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { Product } from './product.model';
+import { products } from './items.data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, MatInputModule, MatCardModule, MatSelectModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first_example';
+  items: Product[] = products;
 }
---
+++ first_example/src/app/items.data.ts
@@ -0,0 +1,39 @@
+import { Product } from "./product.model";
+
+export const products: Product[] = [
+  {
+    name:'Thermal Bottle',
+    price: 3.10,
+    picture: 'assets/images/ThermalBottle.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    name:'Thermal Bottle',
+    price: 2.57,
+    picture: 'assets/images/ThermalBottleAngular.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    name:'Thermal Bottle',
+    price: 2.68,
+    picture: 'assets/images/ThermalBottleBash.webp',
+    type: 'Water Bottle',
+    new: false
+  },
+  {
+    name:'Phone Skin',
+    price: 20,
+    picture: 'assets/images/PhoneSkinAngular.webp',
+    type: 'Phone',
+    new: false
+  },
+  {
+    name:'Copy Book',
+    price: 13.54,
+    picture: 'assets/images/CopyBookAngular.webp',
+    type: 'CopyBook',
+    new: false
+  },
+]
